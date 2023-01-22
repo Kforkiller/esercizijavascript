cc9c378cab756666e0f7e53a58ad8fd26a6d79ac
Your Name <antonio.zappatore.br@gmail.com>
2023-01-22 12:52:19 +0100

    Esercizio class definition

---
+++ exercise-1.js
@@ -0,0 +1,13 @@
+// Class definiti
+
+class Person {
+    firstName = "";
+    lastName = "";
+    constructor( firstName, lastName){
+        this.firstName = firstName
+        this.lastName = lastName
+    }
+}
+
+const developer = new Person('Mario', 'Rossi');
+console.log(developer.firstName + " " + developer.lastName);
