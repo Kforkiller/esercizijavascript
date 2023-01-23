abadbdeafa926620a49d8d95b46430831cc92790
Your Name <antonio.zappatore.br@gmail.com>
2023-01-23 01:09:31 +0100

    Esercizio instanceof

---
+++ exercise-6.js
@@ -0,0 +1,38 @@
+class Square {
+  constructor(side) {
+    this.side = side;
+  }
+}
+
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+}
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+}
+
+class AreaCalculator {
+  static calculate(shapes){
+    if(shapes instanceof Square){
+      return shapes.side * shapes.side 
+        } else if (shapes instanceof Rectangle) {
+          return shapes.width * shapes.height
+            } else if (shapes instanceof Circle) {
+               return 3.14 * shapes.radius * shapes.radius
+    }
+  }
+}
+
+const square = new Square(4);
+const rectangle = new Rectangle(4, 2);
+const circle = new Circle(5);
+
+console.log(AreaCalculator.calculate(square));
+console.log(AreaCalculator.calculate(rectangle));
+console.log(AreaCalculator.calculate(circle));
