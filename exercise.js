c421cce780ff3c1eae70ee0bda05819cf8c467a8
Your Name <antonio.zappatore.br@gmail.com>
2023-02-14 03:04:45 +0100

    risolto

--- exercise.js
+++ exercise.js
@@ -1,30 +1,29 @@
 const isLogged = true;
-function firstPromise (){
-return new Promise((isLogged, reject) => { 
-    if(isLogged == true)
-    {
-        let number = Math.random
-        return number
-    }else{
-        return reject`Error`
+
+function firstPromise(log) {
+    return new Promise((resolve, reject) => {
+    if (log == true) {
+        let number = Math.random()
+        resolve(number)
+    } else {
+        reject(new Error('Error Not logged'))
     }
-})
+    })
 }
 
-function secondPromise (){
-return new Promise((number, reject) => { 
-    if(number > 0.5)
-    {
-        const data ={name : "John", age: "24"}
-        return data
-    }else{
-        return reject
+function secondPromise(number) {
+    console.log(number)
+    return new Promise((resolve, reject) => {
+    if (number > 0.5) {
+        const data = { name: "John", age: "24" }
+        resolve(data)
+    } else {
+        reject(new Error('Error Number'))
     }
-})
+    })
 }
 
-firstPromise()
+firstPromise(isLogged)
     .then((number) => secondPromise(number))
-    .then((number)=> console.log(number))
     .then((solution) => console.log(solution))
     .catch((error) => console.error('ERROR'))
